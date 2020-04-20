b7e3cca22e6327acd11be09e194d28de91c4e385
BartekWSB <62383210+BartekWSB@users.noreply.github.com>
2020-04-20 19:14:44 +0200

    Została wprowadzona pętla służąca do konkretnego wyboru w menu, addliquid. Naniesiony limit pitego płynu

--- Main.py
+++ Main.py
@@ -24,25 +24,54 @@ def addLiquid(): #function that adds liquid and saves it into table in database
     print('1 - Water')
     print('2 - Tea')
     print('3 - Coffee')
-    answer = int(input('What have you drunk? '))
-    if answer == 1:
-        howMuch = int(input('How much ml? '))
-        sql1 = "INSERT INTO " + name + " (liquid, howMuch) VALUES ('Water', %s)"
-        mycursor.execute(sql1, (howMuch,))
-        WaterConnect.commit()
-    elif answer == 2:
-        howMuch = int(input('How much ml? '))
-        sql2 = "INSERT INTO " + name + " (liquid, howMuch) VALUES ('Tea', %s)"
-        mycursor.execute(sql2, (howMuch,))
-        WaterConnect.commit()
-    elif answer == 3:
-        howMuch = int(input('How much ml? '))
-        sql3 = "INSERT INTO " + name + " (liquid, howMuch) VALUES ('Coffee', %s)"
-        mycursor.execute(sql3, (howMuch,))
-        WaterConnect.commit()
-    else:
-        print('Bye!')
-        exit(0)
+    print('4 - exit to menu')
+    answer = '1', '2', '3', '4'
+    add = True
+    while add:
+        try:
+            answer = input('What have you drunk? ')
+            if answer == '1':
+                    howMuch = int(input('How much ml? '))
+                    if howMuch <= 500:
+                        sql1 = "INSERT INTO " + name + " (liquid, howMuch) VALUES ('Water', %s)"
+                        mycursor.execute(sql1, (howMuch,))
+                        WaterConnect.commit()
+                        addLiquid()
+                    else:
+                        print('You can add only 500ml')
+                        addLiquid()
+
+            elif answer == '2':
+                    howMuch = int(input('How much ml? '))
+                    if howMuch <= 500:
+                        sql2 = "INSERT INTO " + name + " (liquid, howMuch) VALUES ('Tea', %s)"
+                        mycursor.execute(sql2, (howMuch,))
+                        WaterConnect.commit()
+                        addLiquid()
+                    else:
+                        print('You can add only 500ml')
+                        addLiquid()
+            elif answer == '3':
+                    howMuch = int(input('How much ml? '))
+                    if howMuch <= 500:
+                        sql3 = "INSERT INTO " + name + " (liquid, howMuch) VALUES ('Coffee', %s)"
+                        mycursor.execute(sql3, (howMuch,))
+                        WaterConnect.commit()
+                        addLiquid()
+                    else:
+                        print('You can add only 500ml')
+                        addLiquid()
+
+            elif answer == '4':
+                    menu()
+
+            else:
+                print('Please, try again.')
+                addLiquid()
+        except: menu()
+    else :
+        print('Please, try again.')
+        addLiquid()
 
 helpdate = datetime.datetime.now() #just a temporary variable
 date = helpdate.strftime("%Y-%m-%d") #variable saves current date to show it by default when user calls addLiquid function
@@ -72,7 +101,7 @@ def showHistory(): #function that shows a history from date which is chosen by u
 
         elif a == "N" or a == 'n':
             print('Turning back to the menu.')
-            break
+            menu()
 
         else:
             print('Wrong symbol. Please, try again.')
@@ -83,14 +112,27 @@ def menu(): #main function which is looped
     print('1 - Add liquid')
     print('2 - Show history')
     print('3 - Exit program')
-    choose = int(input('Please choose: '))
-    if choose == 1:
-        addLiquid()
-    elif choose == 2:
-        showHistory()
-    elif choose == 3:
-        print('Bye!')
-        exit(0)
+    choose = '1', '2', '3'
+    function_menu = True
+    while function_menu:
+        try:
+            choose = input('Please choose: ')
+            if choose == '1':
+                addLiquid()
+            elif choose == '2':
+                showHistory()
+            elif choose == '3':
+                print('Bye!')
+                exit(0)
+                break
+            else:
+                print('Wrong number. Please, try again.')
+                menu()
+        except:
+            exit(0)
+    else:
+        print('Please, try again.')
+        menu()
 
 
 
